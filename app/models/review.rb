070bf4a23834eb6ae037bad7ec05339aee6ded04
angga <antoniusanggakurniawan@gmail.com>
2016-03-18 02:02:38 +0700

    masih di twitter haah

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,10 +8,10 @@ class Review < ActiveRecord::Base
   before_create :post_to_twitter
 
   def post_to_twitter
-    if content.length < 100
+    if content.length < 102
       user.twitter.update(content + " via https://indonesiaku.herokuapp.com")
     else 
-      user.twitter.update(content[0..95] + " ... via https://indonesiaku.herokuapp.com") if user.twitterlink && user.uid
+      user.twitter.update(content[0..98] + " ... via https://indonesiaku.herokuapp.com") if user.twitterlink && user.uid
     end
   end
 end
