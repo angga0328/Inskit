e724ca24d194509d21ac3d1b40d2f9b3d4978005
angga <antoniusanggakurniawan@gmail.com>
2016-03-17 22:46:14 +0700

    merubah consumer api key dan secret twitter

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -240,7 +240,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :twitter, 'tKVBot5vLarsaklyRUEaYt5xs', '7vs5aJViuT8PsLvy0SSJXFAaC5qSa9zyHclWkDkWqo9SbdSf0P'
+  config.omniauth :twitter, ENV['Consumer_API_KEY'], ENV['Consumer_Secret_API']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
