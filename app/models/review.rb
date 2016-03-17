81a6ed94f4c3bbcfc80d2d69641d4e64f26fad31
angga <antoniusanggakurniawan@gmail.com>
2016-03-18 01:56:35 +0700

    cobain post to twitter

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,10 +8,10 @@ class Review < ActiveRecord::Base
   before_create :post_to_twitter
 
   def post_to_twitter
-    if content.length < 123
-      user.twitter.update(content + " via @")
+    if content.length < 100
+      user.twitter.update(content + " via https://indonesiaku.herokuapp.com")
     else 
-      user.twitter.update(content[0..17] + " ... via @") if user.twitterlink && user.uid
+      user.twitter.update(content[0..95] + " ... via https://indonesiaku.herokuapp.com") if user.twitterlink && user.uid
     end
   end
 end
